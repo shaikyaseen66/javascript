2b2c41ddebee0e3f2e1285b683db528d121d7e83
shaikyaseen66 <sy6436920@gmail.com>
2025-04-22 06:20:14 +0000

    conatiiner config

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId="2211345"
+let accountEmail="Yaseen@gamil.com"
+var accountPassword="17738836"
+accountCity="ap"
+
+let accountState;
+
+console.log(accountId)
+ 
+accountEmail="yass@gamil.com"
+accountPassword="4545455"
+accountCity="TS"
+
+console. table({accountEmail,accountPassword, accountCity, accountId, accountState})
+
+
+/*
+so better to not use var 
+because of funtional scope and block scope
+
+*/
