848f14d4237345b41f9ad140b9c8e3088fd6a179
shaikyaseen66 <sy6436920@gmail.com>
2025-04-28 09:28:21 +0000

    commit config

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //  treat js codes as newer version 
+
+// alert( 3 + 3) // we are using nodejs not browser
+ 
+console.log (3+
+    3) // code readability should be high
+console.log ("Yaseen")
+
+    let Name="Yaseen"
+    let age="18"
+ let isLoggedIn=true
+
+
+ 
+
+ // number => 2 to power 53
+ //bigInt
+ //string => ""
+ //boolean => true orr flase
+ //null => stand alone value 
+ // undefined => 
+// symbol => unquie
+
+
+ // object
+ 
+ console.log(typeof"Yaseen")
+ console.log (typeof null) //object
+ console.log(typeof undefined) // undefined
