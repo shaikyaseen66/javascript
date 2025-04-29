77e67a00842043cf07c79b38c953d765cc92f2ac
shaikyaseen66 <sy6436920@gmail.com>
2025-04-29 05:08:28 +0000

    commit config

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+
+let score = false
+  
+//console.log( typeof score)
+//console.log( typeof (score))
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;false => 0
+
+
+let isLoggedIn ="Yaseen"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log (booleanIsLoggedIn);
+
+ // 1 => true ; 0 => false 
+ // "" => false ; "Yaseen" => true
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(someNumber)
